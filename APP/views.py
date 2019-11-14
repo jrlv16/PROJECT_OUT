5e0b7c4750c0404d493648474e95f56a19db2931
jlrv16 <jl062705@sfr.fr>
2019-11-14 03:25:05 +0100

    crud fbv

--- APP/views.py
+++ APP/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic.detail import DetailView
 from django.views.generic import ListView
 from .models import Bonzai
+from .forms import BonzaiForm
 
 # Create your views here.
 # une méthode créée comme une fonction
@@ -27,3 +28,32 @@ class BonzaiListView(ListView):
             return Bonzai.objects.filter(type_arbre=type_arbre) 
         else:
             return Bonzai.objects.all()
+
+def BonzaiCreate(request):
+    template = 'bonzai/bonzai_form.html'
+    form = BonzaiForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('accueil')
+    context = {"form": form}
+    return render(request, template, context)
+
+def bonzai_update(request, pk):
+    template = 'bonzai/bonzai_form.html'
+    bonzai = get_object_or_404(Bonzai, pk=pk)
+    form = BonzaiForm(request.POST or None, instance=bonzai)
+    if form.is_valid():
+        form.save()
+        return redirect('detail_bonzai', pk)
+    context = {"form": form}
+    return render(request, template, context)
+
+def bonzai_delete(request, pk):
+    template = 'bonzai/bonzai_delete.html'
+    bonzai = get_object_or_404(Bonzai, pk=pk)
+    if request.method == 'POST':
+        bonzai.delete()
+        return redirect('accueil')
+    context = {"bonzai": bonzai}
+    return render(request, template, context)
